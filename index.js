31d1c0575d3108ec90bd8923faa55a0466e1e4a3
Prosperity-Mthethwa <nyamboseprosperity@gmail.com>
2024-10-24 11:42:56 +0200

    modified the given parameters

--- index.js
+++ index.js
@@ -6,12 +6,12 @@
  */
 
 // Given Parameters
-const vel = 10000; // velocity (km/h)
-const acc = 3; // acceleration (m/s^2)
+const initialVelocity = 10000; // velocity (km/h)
+const acceleration = 3; // acceleration (m/s^2)
 const time = 3600; // seconds (1 hour)
-const d = 0; // distance (km)
-const fuel = 5000; // remaining fuel (kg)
-const fbr = 0.5; // fuel burn rate (kg/s)
+const initalDistance = 0; // distance (km)
+const initialFuel = 5000; // remaining fuel (kg)
+const fuelBurnRate = 0.5; // fuel burn rate (kg/s)
 
 
 const d2 = d + (vel*time) //calcultes new distance
@@ -19,7 +19,7 @@ const rf = fbr*time //calculates remaining fuel
 const vel2 = calcNewVel(acc, vel, time) //calculates new velocity based on acceleration
 
 // Pick up an error with how the function below is called and make it robust to such errors
-calcNewVel = (vel, acc, time) => { 
+const calcNewVel = () => { 
   return vel + (acc*time)
 }
 
