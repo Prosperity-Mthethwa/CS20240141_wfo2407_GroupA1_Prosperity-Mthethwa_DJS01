ea6c0a16bcc73da81f282c7f38028edb9bae747a
Prosperity-Mthethwa <nyamboseprosperity@gmail.com>
2024-10-24 12:18:24 +0200

    Modified: Index.js

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-/**
- * Debugging Guide
- * 1. Make the code more readable
- * 2. Pick up calculation errors
- * 3. Make these calculations robust such that the calculation does not give an incorrect result, it throws an error to the user if something has gone wrong (parameter used with an incorrect unit of measurement, etc)
- */
-
 // Given Parameters
 const initialVelocity = 10000; // velocity (km/h)
 const acceleration = 3; // acceleration (m/s^2)
@@ -19,7 +12,7 @@ const calcNewVel = ({ initialVelocity, acceleration, time }) => {
     throw new Error('Invalid input. Parameters must be numbers.');
   }
 
-  //initial velocity conversion 
+  //initial velocity conversion from km/h to m/s  
   const initialVelocityInMps = initialVelocity / 3.6; 
 
   //velocity calculation
@@ -35,8 +28,7 @@ const newDistance = initialDistance + initialVelocity * (time / 3600) //calculte
 const remainingFuel = initialFuel - fuelBurnRate * time //calculates remaining fuel
 const newVelocity = calcNewVel({initialVelocity, acceleration, time}); //calculates new velocity based on acceleration
 
-
-
+//Results in the console 
 console.log(`New Velocity: Approximately ${newVelocity.toFixed(0)} km/h after correction.`);
 console.log(`New Distance: Approximately ${newDistance.toFixed(0)} km after correction.`);
 console.log(`Remaining Fuel: Approximately ${remainingFuel.toFixed(0)} kg after correction.`);
